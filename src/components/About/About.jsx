3237bb088ed209c86e31a96417c858369719330e
AAli02 <aali.02p@gmail.com>
2023-02-20 19:17:51 -0500

    mobile nav

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-import "./About.css"
+import "./About.css";
 
 function About() {
   return (
     <section id="section1">
       <div className="section__title">
         <h1 className="title__us">About Us</h1>
+      </div>
+
+      <div className="about__left">
+        
       </div>
     </section>
   );
