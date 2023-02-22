22b9e007ba7e6a22441793c8ae1f7f5b8e626e04
AAli02 <aali.02p@gmail.com>
2023-02-22 13:28:27 -0500

    test6

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import team from "../../assets/team.png";
 
 import "./About.css";
