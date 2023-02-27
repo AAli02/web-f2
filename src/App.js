5701b038fa64536d312b70ab759682b81159fa61
AAli02 <aali.02p@gmail.com>
2023-02-27 14:30:59 -0500

    animation on scroll

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => (
   <div>
     <Router>
       <Routes>
+      <Route path="/Aboutnpm " element={<About About={About} />} />
       </Routes>
     </Router>
     <Nav />
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Aos from "aos";
+
 import team from "../../assets/team.png";
 import pic1 from "../../assets/pic1.png";
 import pic2 from "../../assets/pic2.png";
 import planet from "../../assets/planet.png";
 import check from "../../assets/check.png";
 
+import 'aos/dist/aos.css';
 import "./About.css";
 
-function About() {
+
+
+// const observer = new IntersectionObserver ((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry)
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show')
+//     } else {
+//       entry.target.classList.remove('show')
+//     }
+
+//   });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden')
+// hiddenElements.forEach((el) => observer.observe(el));
+
+
+export const About = () => {
+
+  useEffect(() => {
+    Aos.init({duration: 300});
+  }, [])
+
   return (
     <section id="about">
       <div className="about__wrapper">
@@ -18,10 +44,10 @@ function About() {
       </div>
 
       <div className="about__content">
-        <div className="about__img">
+        <div data-aos="fade-right" className="about__img">
           <img className="team__img" src={pic1} alt="" />
         </div>
-        <div className="about__desc">
+        <div data-aos="fade-left" className="about__desc">
           <h3 className="heading__3">A team of passionate designers and developers</h3>
           <div className="p r__space">
             <strong className="bold__text">Creative digital agency </strong>with solid design and development expertise. 
@@ -41,6 +67,6 @@ function About() {
       </div>
     </section>
   );
-}
+};
 
 export default About;
