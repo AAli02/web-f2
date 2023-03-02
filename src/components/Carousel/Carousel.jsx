8cf33e55658af1aab001e463b8100c966318371d
AAli02 <aali.02p@gmail.com>
2023-03-02 08:13:22 -0500

    might need old version

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,22 +1,9 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import design from "../../assets/designimg.webp";
-import dev from "../../assets/devimg.webp";
-import brand from "../../assets/brandimg.svg";
+import React from 'react'
+import { Container } from 'react-bootstrap'
 
 function Carousel() {
   return (
-    <div className="card">
-      <div className="card__top">
-        <div className="card__bottom">
-
-        </div>
-      </div>
-    </div>
+    <div>Carousel</div>
   )
 }
 
